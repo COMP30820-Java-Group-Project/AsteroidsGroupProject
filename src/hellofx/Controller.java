d781a2190ff78ea514a18ae3ec3378934565e5d0
Jack Mulligan <mulligja1@gmail.com>
2022-03-30 09:42:31 +0100

    Moved render logic into Main.java

--- src/hellofx/Controller.java
+++ src/hellofx/Controller.java
@@ -2,15 +2,23 @@ package hellofx;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.shape.*;
+import javafx.scene.paint.*;
+
 
 public class Controller {
 
     @FXML
-    private Label label;
+    private Rectangle myRect;
+
+    //private Label label;
 
     public void initialize() {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        label.setText("Hello, JavaFX " + javafxVersion + "\nRunning on Java " + javaVersion + ".");
+        // String javaVersion = System.getProperty("java.version");
+        // String javafxVersion = System.getProperty("javafx.version");
+        // label.setText("Hello, JavaFX " + javafxVersion + "\nRunning on Java " + javaVersion + ".");
+
+        myRect = new Rectangle(25,25,250,250);
+        myRect.setFill(Color.PINK);
     }
 }
--- src/hellofx/Main.java
+++ src/hellofx/Main.java
@@ -1,22 +1,28 @@
 package hellofx;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.scene.layout.Pane;
 
 public class Main extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("hellofx.fxml"));
-        primaryStage.setTitle("Hello Asteroids");
-        primaryStage.setScene(new Scene(root, 400, 300));
-        primaryStage.show();
+    // Lets us add children
+    private Pane root = new Pane();
+
+    private Parent createContent() {
+        root.setPrefSize(1000, 900);
+        return root;
     }
 
-
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        primaryStage.setTitle("Hello Asteroids");
+        primaryStage.setScene(new Scene(createContent()));
+        primaryStage.show();
+    }
     public static void main(String[] args) {
         launch(args);
     }
